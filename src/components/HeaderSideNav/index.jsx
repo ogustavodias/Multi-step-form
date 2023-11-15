f0b7bd07b17d923c5393062b5975ae6d8c9f4098
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-11-14 21:16:55 -0300

    Estrutura inicial do projeto

---
+++ src/components/HeaderSideNav/index.jsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+const HeaderSideNav = () => {
+  return <div className={styles.HeaderSideNav}>HeaderSideNav</div>;
+};
+
+export default HeaderSideNav;
---
+++ src/components/form/index.jsx
@@ -0,0 +1,5 @@
+const Form = ({step}) => {
+  return <div>{step}</div>;
+};
+
+export default Form;
