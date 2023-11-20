e3232ce6aec5d2110c43c32b98d7344136b648d4
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-11-20 12:25:45 -0300

    Projeto reestruturado novamente para facilitar o desenvolvimento da aplicação - 'Se você não se planeja, você está planejando falhar' - Benjamin Franklin

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import "./global.css";
 import styles from "./styles.module.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
-import StepOne from "./pages/StepOne/index";
 
 const App = () => {
   return (
     <div className={styles.App}>
-      <BrowserRouter>
         <NavigationBar />
-        <Routes>
-          <Route exact path="/" element={<StepOne />} />
-          <Route exact path="/2" element={<StepOne />} />
-          <Route exact path="/3" element={<StepOne />} />
-          <Route exact path="/4" element={<StepOne />} />
-        </Routes>
-      </BrowserRouter>
     </div>
   );
 };
--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,15 +1,9 @@
-import NavigationLink from "./NavigationLink";
 import styles from "./styles.module.css";
 
 const NavigationBar = () => {
   return (
     <nav className={styles.NavigationBar}>
-      <ul>
-        <li><NavigationLink step={1} path={"/"}>YOUR INFO</NavigationLink></li>
-        <li><NavigationLink step={2} path={"/2"}>SELECT PLAN</NavigationLink></li>
-        <li><NavigationLink step={3} path={"/3"}>ADD-ONS</NavigationLink></li>
-        <li><NavigationLink step={4} path={"/4"}>SUMMARY</NavigationLink></li>
-      </ul>
+      <ul></ul>
     </nav>
   );
 };
