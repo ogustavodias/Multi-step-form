baf39beed4c860ddb9dd086b5e4f2a7d3ba45ae7
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-11-23 20:53:37 -0300

    Feat - Adicionada a lógica para exibição do FormControls e seus respectivos botões a depender do step atual

--- src/components/Button/Confirm/index.jsx
+++ src/components/Button/Confirm/index.jsx
@@ -1,8 +1,19 @@
+// Styles
 import styles from "./styles.module.css";
 
+// Context
+import { AppContext } from "../../../context";
+import { useContext } from "react";
+
 const Confirm = () => {
+  const { setStep } = useContext(AppContext);
+
+  function handleConfirm() {
+    setStep(5);
+  }
+
   return (
-    <button className={styles.Confirm} type="submit">
+    <button className={styles.Confirm} type="button" onClick={handleConfirm}>
       Confirm
     </button>
   );
--- src/components/FormControls/index.jsx
+++ src/components/FormControls/index.jsx
@@ -4,11 +4,19 @@ import styles from "./styles.module.css";
 // Button global component
 import Button from "../Button";
 
+// Context
+import { useContext } from "react";
+import { AppContext } from "../../context";
+
 const FormControls = () => {
+  const { step } = useContext(AppContext);
+
+  if (step === 5) return null;
   return (
     <div className={styles.FormControls}>
-      <Button type={"back"} />
-      <Button type={"next"} />
+      {step > 1 && <Button type={"back"} />}
+      {step < 4 && <Button type={"next"} />}
+      {step === 4 && <Button type={"confirm"} />}
     </div>
   );
 };
